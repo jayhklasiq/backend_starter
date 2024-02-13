f821b4142a38e8ba1f07012d26bfb2e24e6d7b4c
jayhklasiq <joshvolx@gmail.com>
2024-02-13 22:33:47 +0000

    updated js files and created new ones as well

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
-const static = require("./routes/static")
+// const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -25,7 +26,10 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes: All resources exported from
  static.js are now being used by the application.
  *************************/
-app.use(static)
+// app.use("static")
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
 //Index route
 app.get("/", baseController.buildHome)
 
--- utilities/index.js
+++ utilities/index.js
@@ -8,15 +8,15 @@ const Util = {}
 Util.getNav = async function (req, res, next) {
   let data = await invModel.getClassifications()
   let list = "<ul>"
-  list += `<li><a href="/" title="Home Page">Home</a></li>`
+  list += '<li><a href="/" title="Home page">Home</a></li>'
   data.rows.forEach((row) => {
     list += "<li>"
     list +=
-      `<a href="/inv/type/` +
-      row.classsification_id +
-      `" title="See our Inventory of ` +
+      '<a href="/inv/type/' +
+      row.classification_id +
+      '" title="See our inventory of ' +
       row.classification_name +
-      'veehicles">' +
+      ' vehicles">' +
       row.classification_name +
       "</a>"
     list += "</li>"
@@ -26,3 +26,46 @@ Util.getNav = async function (req, res, next) {
 }
 
 module.exports = Util
+
+
+
+
+
+
+
+
+
+/* **************************************
+* Build the classification view HTML
+* ************************************ */
+Util.buildClassificationGrid = async function (data) {
+  let grid; // Initialize grid variable
+  if (data.length > 0) { // Check if data array is not empty
+    grid = '<ul id="inv-display">'; // Start building the unordered list
+    data.forEach(vehicle => { // Iterate through each vehicle in the data array
+      grid += '<li>'; // Start building list item
+      // Add link to detail page with image and title attributes
+      grid += '<a href="../../inv/detail/' + vehicle.inv_id +
+        '" title="View ' + vehicle.inv_make + ' ' + vehicle.inv_model +
+        'details"><img src="' + vehicle.inv_thumbnail +
+        '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model +
+        ' on CSE Motors" /></a>';
+      grid += '<div class="namePrice">'; // Start building container for name and price
+      grid += '<hr />'; // Add horizontal line
+      grid += '<h2>'; // Start building heading for vehicle name
+      // Add link to detail page with vehicle make and model as text
+      grid += '<a href="../../inv/detail/' + vehicle.inv_id +
+        '" title="View ' + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' +
+        vehicle.inv_make + ' ' + vehicle.inv_model + '</a>';
+      grid += '</h2>'; // End heading
+      // Display vehicle price with formatting
+      grid += '<span>$' + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>';
+      grid += '</div>'; // End namePrice container
+      grid += '</li>'; // End list item
+    });
+    grid += '</ul>'; // End unordered list
+  } else {
+    grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'; // If no data, display notice
+  }
+  return grid; // Return the constructed HTML grid
+}
